dc77f26cf0eb1efed1cbdd34ee091460bfb3f069
Fernanda Pinho <fbp@ecomp.poli.br>
2022-06-27 08:20:18 -0300

    fist commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// o content guarda as pastas onde o tailwond será ultilizado
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
