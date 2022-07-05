1fbd1020a3286ff9f0da17879a0e132e44501034
Fernanda Pinho <fbp@ecomp.poli.br>
2022-07-05 07:46:03 -0300

    organize grapgql structure

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,30 +1,10 @@
 import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql`
-    query {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-        id
-        availableAt
-        slug
-        title
-        lessonType
-    }
-}
-`
-
-interface GetLessonsQueryResponse {
-    lessons: {
-        id: string;
-        title: string;
-        slug:string;
-        availableAt: Date;
-        lessonType: 'live' | 'class'
-    }[]
-}
 export function SideBar() {
 
-    const {data} = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+    const {data} = useGetLessonsQuery()
     console.log(data);
      
     return(
