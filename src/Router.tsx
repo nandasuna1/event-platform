b520dbd3a909937e7a7ec39492074210e930c1d5
Fernanda Pinho <fbp@ecomp.poli.br>
2022-07-05 18:26:55 -0300

    refact: change home route

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,11 +5,9 @@ import { Subscribe } from "./pages/Suscribe";
 export function Router() {
     return (
         <Routes>
-            <Route path="/" element={<Evento/>}/>
+            <Route path="/" element={<Subscribe />} />
             <Route path="/event" element={<Evento/>}></Route>
             <Route path="/event/lesson/:slug" element={<Evento/>}></Route>
-            <Route path="/subscribe" element={<Subscribe />} />
-
         </Routes>
     )
 } 
